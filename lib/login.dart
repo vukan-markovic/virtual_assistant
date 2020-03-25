2b58fa9cf25538944b80c5ced0194861a8c1b9f0
Vukan-Markovic <vukan.markovic97@gmail.com>
2020-03-25 18:51:29 +0100

    commit

---
+++ lib/login.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:virtual_assistant/chatbot.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+class SignInPage extends StatefulWidget {
+  final String title = 'Login';
+
+  @override
+  State<StatefulWidget> createState() => SignInPageState();
+}
+
+class SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(20.0),
+            scrollDirection: Axis.vertical,
+            children: <Widget>[_GoogleSignInSection()],
+          ),
+        );
+      }),
+      backgroundColor: Colors.redAccent,
+    );
+  }
+}
+
+class _GoogleSignInSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _GoogleSignInSectionState();
+}
+
+class _GoogleSignInSectionState extends State<_GoogleSignInSection> {
+  bool _success;
+  String _userID;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: const Text('Sign in to chat with assistant'),
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton.icon(
+            onPressed: () async {
+              _signInWithGoogle();
+            },
+            icon: Image.asset(
+              "img/google.png",
+              width: 30,
+              height: 30,
+            ),
+            label: const Text('Sign in with Google'),
+            color: Colors.red,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton.icon(
+            onPressed: () async {
+              _signInWithTwitter();
+            },
+            icon: Image.asset(
+              "img/twitter.png",
+              width: 30,
+              height: 30,
+            ),
+            label: const Text('Sign in with Twitter'),
+            color: Colors.red,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton.icon(
+            onPressed: () async {
+              _signInWithFacebook();
+            },
+            icon: Image.asset(
+              "img/facebook.png",
+              width: 30,
+              height: 30,
+            ),
+            label: const Text('Sign in with Facebook'),
+            color: Colors.red,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            _success == null
+                ? ''
+                : (_success ? 'Successfully signed in!' : 'Sign in failed!'),
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _signInWithGoogle() async {
+    FirebaseUser user;
+    
+    try {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    user =
+        (await _auth.signInWithCredential(credential)).user;
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    } catch(_) {}
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+        _pushPage(context, HomePageDialogflowV2(user: user));
+      } else {
+        _success = false;
+      }
+    });
+  }
+
+  void _signInWithFacebook() async {
+    FirebaseUser user;
+
+    try {
+      final facebookLogin = FacebookLogin();
+      final result = await facebookLogin.logIn(['email']);
+
+      final AuthCredential credential = FacebookAuthProvider.getCredential(
+          accessToken: result.accessToken.token);
+      FirebaseUser user =
+          (await _auth.signInWithCredential(credential)).user;
+      assert(user.email != null);
+      assert(user.displayName != null);
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
+    } catch (_) {}
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+        _pushPage(context, HomePageDialogflowV2(user: user));
+      } else
+        _success = false;
+    });
+  }
+
+  void _signInWithTwitter() async {
+    FirebaseUser user;
+
+    try {
+      var twitterLogin = new TwitterLogin(
+        consumerKey: '44mkQlM6NuSJLteObmHosVior',
+        consumerSecret: 'YSAGPCXOKTQm333pxKBlr98AvX3hDtD2cGHoLpnfZZb6ZUrSQf',
+      );
+
+      final TwitterLoginResult result = await twitterLogin.authorize();
+      final AuthCredential credential = TwitterAuthProvider.getCredential(
+          authToken: result.session.token,
+          authTokenSecret: result.session.secret);
+      user = (await _auth.signInWithCredential(credential)).user;
+      assert(user.email != null);
+      assert(user.displayName != null);
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
+    } catch (_) {}
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+        _pushPage(context, HomePageDialogflowV2(user: user));
+      } else
+        _success = false;
+    });
+  }
+
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute<void>(builder: (_) => page),
+    );
+  }
+}
---
+++ lib/message.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.name, this.type, this.photo});
+
+  final String text;
+  final String name;
+  final bool type;
+  final String photo;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      new Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: new CircleAvatar(
+          child: new Image.asset("img/ic_launcher.png"),
+          // backgroundColor: Colors.transparent
+        ),
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(this.name,
+                style: new TextStyle(fontWeight: FontWeight.bold)),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: 10.0, bottom: 10.0, left: 10.0, right: 10.0),
+                child: new Text(
+                  text,
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text(this.name, style: Theme.of(context).textTheme.subhead),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: 10.0, bottom: 10.0, left: 10.0, right: 10.0),
+                child: new Text(
+                  text,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      new Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: new CircleAvatar(child: new Image.network(this.photo)),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+  }
+}
